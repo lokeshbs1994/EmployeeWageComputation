62cffb244418d1fbeb4c50734ad7a00d4965a7e7
lokeshbs1994 <lokeshbs619@gmail.com>
2022-01-19 11:54:47 +0530

    Added: Storing daily wage inside  the dictionary

--- empWage.sh
+++ empWage.sh
@@ -8,6 +8,8 @@ maxWorkingHrs=100;
 maxWorkingDays=20;
 totalEmpHrs=0;
 totalWorkingDays=0;
+declare -A dailyWage
+
 function  getWorkingHrs () {
 	case $1 in
       $isFullTime)
@@ -33,9 +35,11 @@ do
 	empCheck=$((RANDOM%3));
 	empHrs=$( getWorkingHrs $empCheck )
 	totalEmpHrs=$(($totalEmpHrs+$empHrs));
-	dailyWage[$totalWorkingDays]=$( getEmpWageForADay $empHrs )
+	dailyWage["Day "$totalWorkingDays]=$( getEmpWageForADay $empHrs )
 
 done
 totalSalary=$(($totalEmpHrs*$empRatePerHrs));
 echo $totalSalary
+echo ${!dailyWage[@]}
 echo ${dailyWage[@]}
+echo "last day salary " ${dailyWage["Day "20]}
